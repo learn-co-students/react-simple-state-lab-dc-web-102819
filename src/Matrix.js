4b2bbb4abca26e02e33eeef2314953ec671a9adf
Kyle <kylepcole@gmail.com>
2020-01-06 12:34:45 -0500

    wip

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+// import Cell from './Cell'
 
 export default class Matrix extends Component {
   
-  genRow = (vals) => (
-    vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
-  )
+  genRow = (vals) => {
+    // console.log(vals)
+    vals.map(val => {
+      consosle.log(val)
+      consosle.log("creating cells")
+      <div className="cell"></div>
+    }) // replace me and render a cell component instead!
+  }
   
-  genMatrix = () => (
-    this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
-  )
+  genMatrix = () => {
+    // console.log(this.props.values)
+    return this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
+  }
   
   render() {
     return (
@@ -17,5 +24,19 @@ export default class Matrix extends Component {
       </div>
     )
   }
-  
+}
+
+Matrix.defaultProps = {
+  values: [
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+  ]
 }
